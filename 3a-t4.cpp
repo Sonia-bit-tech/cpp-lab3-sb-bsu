843d63a70481e75a9c7aa2c3d2257f5abc1c8567
Sonia <sonyakon07@gmail.com>
2024-11-05 13:32:55 +0300

    Update 3a-t4.cpp

--- 3a-t4.cpp
+++ 3a-t4.cpp
@@ -1,9 +1,19 @@
-#include<iostream> using namespace std;
+#include<iostream>
+using namespace std; 
+
 int main()
 {
 int k, i = 1, sum_k = 0, number;
-cout << " Enter the amount of integers to sum = "; cin >> k; do {
-cout << "Enter integer nr. " << i << ": "; cin >> number;
+while (true) {
+cout << " Enter the amount of integers to sum = "; cin >> k;
+if (k <= 0) { cout << "Enter >0 number" << endl; continue; }
+break;
+}
+do {
+cout << "Enter integer nr. " << i << ": "; 
+cin >> number;
 sum_k += number; i++;
-} while (i <= k); cout<<"Thetotalsumof"<<k<<"integersis:" <<sum_k; return 0;
+} while (i <= k);
+cout << " The total sum of " << k << " integers is: "  << sum_k;
+return 0;
 }
